dd962acd958e0581458a9917ecf090fdfdf652aa
paulspokepulls <paulspokepulls@gmail.com>
2026-08-31 15:37:34 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import {createRoot} from "react-dom/client";
 import {createClient} from "@supabase/supabase-js";
 import "./styles.css";
 
+
 const url=import.meta.env.VITE_SUPABASE_URL;
 const key=import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 const supabase=url&&key?createClient(url,key):null;
